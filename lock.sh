54f3b5e1f4e2fd174da730e681297fc764b20f47
katarafett <arpperry19@gmail.com>
2025-04-10 08:42:33 -0400

    cursor update

--- lock.sh
+++ lock.sh
@@ -3,7 +3,6 @@
 if [ $(cat /etc/hostname) = "infinity" ] ; then
 	exec swayidle -w \
 		 timeout 300 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4' \
-         timeout 600 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
-		 timeout 10 'if pgrep swaylock; then swaymsg "output * power off"; fi' resume 'swaymsg "output * power on"' \
+         timeout 310 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
          lock 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4'
 fi
