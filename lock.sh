0d919c4ed3a5fed6d83a7b3ffdd69d7a681557d2
katarafett <arpperry19@gmail.com>
2025-04-21 10:12:47 -0400

    fixed cmus init launch

--- lock.sh
+++ lock.sh
@@ -2,7 +2,8 @@
 
 if [ $(cat /etc/hostname) = "infinity" ] ; then
 	exec swayidle -w \
-		 timeout 300 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4' \
-         timeout 310 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
-         lock 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4'
+         timeout 300 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
+		 timeout 310 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4' \
+         lock 'swaylock -f -c 000000 --ring-color 454545 --key-hl-color 723ee4' \
+		 timeout 900 'poweroff'
 fi
